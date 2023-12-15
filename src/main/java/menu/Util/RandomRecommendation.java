55a2088ebfb3a0f022892d9ebb22f73795fcc92d
masonkimseoul <masonkimseoul@gmail.com>
2023-12-15 16:49:46 +0900

    refactor : 랜덤 추천 메서드를 public으로 변경

--- src/main/java/menu/Util/RandomRecommendation.java
+++ src/main/java/menu/Util/RandomRecommendation.java
@@ -5,11 +5,11 @@ import camp.nextstep.edu.missionutils.Randoms;
 import menu.Domain.Menu;
 
 public class RandomRecommendation {
-    private static String recommendCategory() {
+    public static String recommendCategory() {
         return Menu.fromCategoryNumber(Randoms.pickNumberInRange(1, 5));
     }
 
-    private static String recommendMenu(List<String> menus) {
+    public static String recommendMenu(List<String> menus) {
         return Randoms.shuffle(menus).get(0);
     }
 }
