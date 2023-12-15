8981b09b571d7c7c1a77bf871d61756ba64e9fbc
masonkimseoul <masonkimseoul@gmail.com>
2023-12-15 16:50:27 +0900

    refactor : Coach 클래스에 RecommendedMenu 클래스 삽입

--- src/main/java/menu/Domain/Coach.java
+++ src/main/java/menu/Domain/Coach.java
@@ -6,10 +6,12 @@ import java.util.List;
 public class Coach {
     private final String name;
     private final List<String> menusForAvoid;
+    private final RecommendedMenu recommendedMenu;
 
     public Coach(String name, List<String> menusHate) {
         this.name = name;
         this.menusForAvoid = new ArrayList<>();
+        this.recommendedMenu = new RecommendedMenu();
         menusForAvoid.addAll(menusHate);
     }
 
@@ -19,5 +21,17 @@ public class Coach {
 
     public boolean isMenusForAvoid(String menuName) {
         return this.menusForAvoid.contains(menuName);
+    }
+
+    public void addRecommendedMenu(String menu) {
+        this.recommendedMenu.addRecommendedMenu(menu);
+    }
+
+    public List<String> getRecommendedMenu() {
+        return this.recommendedMenu.getRecommendedMenu();
+    }
+
+    public boolean isAlreadyRecommended(String menu) {
+        return this.recommendedMenu.isAlreadyRecommended(menu);
     }
 }
